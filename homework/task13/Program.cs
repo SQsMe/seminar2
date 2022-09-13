e0df5658a2937cf97167775c3a9ab35391bff52a
SQsMe <goldswag1996@mail.ru>
2022-09-13 03:26:59 +0300

    123

--- homework/task13/Program.cs
+++ homework/task13/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+// Console.Write("Введи число: ");
+// int anyNumber = Convert.ToInt32(Console.ReadLine()); //задаем переменную anyNumber
+// string anyNumberText = Convert.ToString(anyNumber); // переводим в ряд чисел
+// if (anyNumberText.Length > 2){
+//  Console.WriteLine($"третья цифра" + anyNumberText[2]); //тут не ставятся {} внутри совместно с []}
+// else {
+//  Console.WriteLine("третьей цифры нет");
+// }
+int n = new Random().Next(100, 10000);
+if (n < 100){                           // проверяем на трехзначность
+  System.Console.WriteLine("Третьей цифры нет");
+}
+else{
+  int result = n / 100 % 10; // третье число
+  System.Console.WriteLine($"Третья цифра цисла {n} --> {result}");
+}
